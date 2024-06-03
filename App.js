e6083411d728575cb8493ec503de35482bf48726
Khushbu712 <khushbuchoudhary2523@gmail.com>
2024-06-03 12:50:28 +0530

    app.js with routes

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import About from './pages/About';
-import Home from './pages/Home';
-import Item from './pages/Item';
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+// createBR takes the list of multiple obj(based on no of routes)
+import About from "./pages/About";
+import Item from "./pages/Item";
+
+// here we have just created the routes not provided the routes to  the application
 
 function App() {
-  const [page,setPage] = useState('home');
+
+  const router = createBrowserRouter([
+    {path: "/",element:<Home/>},
+    {path:"/about",element:<About/>},
+    {path:"/item",element:<Item/>}
+  ])
+
   return (
     <>
-    <Navbar setPage={setPage}/>
-    {page === "home"&& <Home/>}
-    {page === "about" && <About/>}
-    {page === "items" && <Item/>}
+   <RouterProvider  router={router}/>
     </>
   )
 }
