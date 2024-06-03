e4756cba589a35c622252521a6ad6dc69b3848e3
Khushbu712 <khushbuchoudhary2523@gmail.com>
2024-06-03 17:07:13 +0530

    routes & outlet

--- App.js
+++ App.js
@@ -1,22 +1,41 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider ,createRoutesFromElements,Route} from "react-router-dom";
 import Home from "./pages/Home";
 // createBR takes the list of multiple obj(based on no of routes)
 import About from "./pages/About";
 import Item from "./pages/Item";
+import {Navbar} from "./components/Navbar"
 
 // here we have just created the routes not provided the routes to  the application
 
 function App() {
 
+  // const routes = createRoutesFromElements(
+  //   <>
+  //   <Route path="/" element={<Home/>}/>
+  //   <Route path="/about" element={<About/>}/>
+  //   <Route path="/item" element={<Item/>}/>
+
+  //   </>
+  // )
+
+  // const router = createBrowserRouter(routes);
+
+
   const router = createBrowserRouter([
-    {path: "/",element:<Home/>},
-    {path:"/about",element:<About/>},
-    {path:"/item",element:<Item/>}
-  ])
+    {path:"/",
+    element:<Navbar />,
+     children:[
+    { index:true ,element:<Home/>},
+    {path:"/about", element:<About/>},
+    {path:"/item", element:<Item/>}
+    ],
+  },
+    ]
+  )
 
   return (
     <>
-   <RouterProvider  router={router}/>
+   <RouterProvider router={router}/>
     </>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,24 @@
-function Navbar({setPage}) {
+import { Link,Outlet } from "react-router-dom"
+
+export function Navbar() {
     return (
         <>
         <div className="nav">
-        <h4 onClick={()=> setPage('home')}>Home</h4>
-        <h4 onClick={()=> setPage('about')}>About</h4>
-        <h4 onClick={()=> setPage('items')}>Items</h4>
+        <Link to='/'>
+        <h4>Home</h4>
+        </Link>&nbsp;&nbsp;
+
+        <Link to='/list'>
+        <h4>List</h4>
+        </Link>&nbsp;&nbsp;
+
+        <Link to='/contact'>
+        <h4>Contact</h4>
+        </Link>
 
         </div>
+        <Outlet/>
         </>
     )
 }
 
-export default Navbar
